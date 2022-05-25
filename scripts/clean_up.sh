e57f5c0b42516de69ed77f005855f725eb3f627f
Darko Gjorgjijoski <dg@darkog.com>
2022-05-25 20:45:38 +0000

    Remove examples from tcpdf in clean up script

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -14,6 +14,7 @@ if [ $fonts_count -gt 3 ]; then
     rm -rf ../vendor/tecnickcom/tcpdf/fonts/*
     cp _tmp/fonts/courier.* ../vendor/tecnickcom/tcpdf/fonts
     cp _tmp/fonts/helvetica.* ../vendor/tecnickcom/tcpdf/fonts
+    rm -rf ../vendor/tecnickcom/tcpdf/examples
     rm -rf _tmp
     echo 'Clean up done.';
 else
