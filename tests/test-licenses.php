8660a1b9344155d274e8137a5d3a86a410b72f77
Darko Gjorgjijoski <dg@darkog.com>
2022-10-04 14:02:10 +0200

    Init tests

--- tests/test-licenses.php
+++ tests/test-licenses.php
@@ -8,12 +8,12 @@
 /**
  * Sample test case.
  */
-class SampleTest extends WP_UnitTestCase {
+class LicensesTest extends WP_UnitTestCase {
 
 	/**
 	 * A single example test.
 	 */
-	public function test_sample() {
+	public function test_licenses() {
 		// Replace this with some actual testing code.
 		$this->assertTrue( true );
 	}
