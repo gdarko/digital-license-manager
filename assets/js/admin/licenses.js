0f60cfc52548d9304a6d4d8f1e4b07260a2b6c32
Darko Gjorgjijoski <dg@darkog.com>
2023-02-12 19:09:39 +0100

    Clean up and reorganize js a bit

--- assets/js/admin/licenses.js
+++ assets/js/admin/licenses.js
@@ -2,14 +2,25 @@ window.DLM = window.hasOwnProperty('DLM') ? window.DLM : {};
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    window.DLM.Licenses = function () {
+    var Licenses = function () {
         this.setupListeners();
     }
 
     /**
-     * Set up the listeners
+     * Set up all listeners
      */
-    window.DLM.Licenses.prototype.setupListeners = function () {
+    Licenses.prototype.setupListeners = function () {
+        this.setupFormFields();
+        this.setupLicenseKeyToggle();
+        this.setupLicenseKeyClipboard();
+    }
+
+    /**
+     * Set up the form fields listeners
+     * - Tom-select
+     * - Flatpickr
+     */
+    Licenses.prototype.setupFormFields = function () {
 
         var importLicenseProduct = document.querySelector('select#bulk__product');
         var importLicenseOrder = document.querySelector('select#bulk__order');
@@ -162,6 +173,164 @@ document.addEventListener("DOMContentLoaded", function (event) {
         }
     }
 
+    /**
+     * Set up license key ajax based toggle
+     */
+    Licenses.prototype.setupLicenseKeyToggle = function () {
+        var self = this;
+
+        // Single key toggle
+        var licenseToggles = document.querySelectorAll('.dlm-license-key-toggle');
+        if (licenseToggles) {
+            for (var i = 0; i < licenseToggles.length; i++) {
+                licenseToggles[i].addEventListener('click', function (e) {
+                    self.handleLicenseKeysToggle([this.closest('tr')], this.classList.contains('dlm-license-key-show'));
+                }.bind(licenseToggles[i]));
+            }
+        }
+
+        // Toggle all
+        var licensesToggleAllBtns = document.querySelectorAll('.dlm-license-keys-toggle-all');
+        if (licensesToggleAllBtns) {
+            for (var i = 0; i < licensesToggleAllBtns.length; i++) {
+                licensesToggleAllBtns[i].addEventListener('click', function (e) {
+                    var elements = this.closest('td').querySelectorAll('.dlm-license-list li');
+                    self.handleLicenseKeysToggle(elements, this.classList.contains('dlm-license-keys-show-all'));
+                }.bind(licensesToggleAllBtns[i]))
+            }
+        }
+
+    }
+
+    /**
+     * Setup license key cliboard
+     */
+    Licenses.prototype.setupLicenseKeyClipboard = function () {
+        var licenseKeys = document.querySelectorAll('.license_key .dlm-placeholder, .dlm-license-list .dlm-placeholder');
+        if (licenseKeys) {
+            for (var i = 0; i < licenseKeys.length; i++) {
+                licenseKeys[i].addEventListener('click', this.copyLicenseToClipboard.bind(licenseKeys[i]));
+            }
+        }
+    }
+
+    /**
+     * Handle license toggle
+     * @param {Array} elements
+     * @param {Boolean} isShow
+     */
+    Licenses.prototype.handleLicenseKeysToggle = function (elements, isShow) {
+        var self = this
+        if (isShow) {
+            var licenseIds = [];
+            for (var i = 0; i < elements.length; i++) {
+                var placeholder = elements[i].querySelector('.dlm-placeholder');
+                if (placeholder) {
+                    licenseIds.push(parseInt(placeholder.dataset.id));
+                }
+                self.toggleLicenseKeySpinner(elements[i], true);
+            }
+            var http = new window.DLM.Http();
+            http.post(ajaxurl, {
+                data: {
+                    action: 'dlm_show_all_license_keys',
+                    show_all: DLM_MAIN.show_all,
+                    ids: JSON.stringify(licenseIds)
+                },
+                success: function (response, responseStatus, responseHeaders) {
+                    for (var id in response) {
+                        var licenseKey = document.querySelector('.dlm-placeholder[data-id="' + id + '"]');
+                        licenseKey.classList.remove('empty');
+                        licenseKey.innerHTML = response[id];
+                    }
+                },
+                error: function (response, responseStatus, responseHeaders) {
+                    alert(response);
+                },
+                complete: function () {
+                    for (var i = 0; i < elements.length; i++) {
+                        self.toggleLicenseKeySpinner(elements[i], false)
+                    }
+                }
+            });
+        } else {
+            for (var i = 0; i < elements.length; i++) {
+                var licenseKey = elements[i].querySelector('.dlm-placeholder');
+                if (licenseKey) {
+                    licenseKey.innerHTML = '';
+                    licenseKey.classList.add('empty');
+                }
+            }
+        }
+
+    }
+
+
+    /**
+     *
+     * @param element
+     * @param state
+     */
+    Licenses.prototype.toggleLicenseKeySpinner = function (element, state) {
+        element.querySelector('.dlm-spinner').style.opacity = state ? 1 : 0;
+    }
+
+
+    /**
+     * Copy license to clipboard
+     * @param e
+     */
+    Licenses.prototype.copyLicenseToClipboard = function (e) {
+
+        var el = this;
+
+        if (!el) {
+            return;
+        }
+        var str = el.innerHTML;
+
+        if (str.length === 0) {
+            return;
+        }
+
+        var textArea = document.createElement('textarea');
+        textArea.value = str;
+        textArea.setAttribute('readonly', '');
+        textArea.style.position = 'absolute';
+        textArea.style.left = '-9999px';
+        document.body.appendChild(textArea);
+        var selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+        if (selected) {
+            document.getSelection().removeAllRanges();
+            document.getSelection().addRange(selected);
+        }
+
+        // Display info
+        var copied = document.createElement('div');
+        copied.classList.add('dlm-clipboard');
+        copied.style.position = 'absolute';
+        copied.style.left = e.clientX.toString() + 'px';
+        copied.style.top = (window.pageYOffset + e.clientY).toString() + 'px';
+        copied.innerText = document.querySelector('.dlm-txt-copied-to-clipboard').innerText.toString();
+        document.body.appendChild(copied);
+
+        setTimeout(function () {
+            copied.style.opacity = '0';
+        }, 700);
+        setTimeout(function () {
+            document.body.removeChild(copied);
+        }, 1500);
+    }
+
+
+    /**
+     * Initialize
+     * @type {Licenses}
+     */
+    window.DLM.Licenses = Licenses;
     new window.DLM.Licenses();
 
 });
--- includes/Boot.php
+++ includes/Boot.php
@@ -137,11 +137,11 @@ class Boot extends Singleton {
 		 * Page specific
 		 */
 		wp_register_script( 'dlm_licenses_page', DLM_JS_URL . 'admin/licenses.js', array( 'jquery', 'dlm_select', 'dlm_flatpickr' ), $this->version );
-		wp_register_script( 'dlm_generators_page', DLM_JS_URL . 'admin/generators.js', array( 'jquery', 'dlm_select' ), $this->version );
-		wp_register_script( 'dlm_activations_page', DLM_JS_URL . 'admin/activations.js', array( 'jquery', 'dlm_select' ), $this->version );
+		wp_register_script( 'dlm_generators_page', DLM_JS_URL . 'admin/generators.js', array( 'dlm_select' ), $this->version );
+		wp_register_script( 'dlm_activations_page', DLM_JS_URL . 'admin/activations.js', array( 'dlm_select' ), $this->version );
 		wp_register_script( 'dlm_settings_page', DLM_JS_URL . 'admin/settings.js', array( 'dlm_utils' ), $this->version );
 		wp_register_style( 'dlm_settings_page', DLM_CSS_URL . 'admin/settings.css', array(), $this->version, 'all' );
-		wp_register_script( 'dlm_products_page', DLM_JS_URL . 'admin/products.js', array( 'jquery' ), $this->version );
+		wp_register_script( 'dlm_products_page', DLM_JS_URL . 'admin/products.js', array(), $this->version );
 		wp_register_style( 'dlm_manage_page', DLM_CSS_URL . 'admin/manage.css', array(), $this->version, 'all' );
 		wp_register_script( 'dlm_tools_page', DLM_JS_URL . 'admin/tools.js', array( 'dlm_http' ), $this->version );
 		wp_register_style( 'dlm_tools_page', DLM_CSS_URL . 'admin/tools.css', array(), $this->version, 'all' );
@@ -178,6 +178,7 @@ class Boot extends Singleton {
 		$isActivations = $hook === 'license-manager_page_dlm_activations';
 		$isSettings    = $hook === 'license-manager_page_dlm_settings';
 		$isProducts    = in_array( $hook, array( 'post.php', 'post-new.php' ) ) && 'product' === $post_type;
+		$isOrder       = in_array( $hook, array( 'post.php', 'post-new.php' ) ) && 'shop_order' === $post_type;
 		$isManage      = $isLicenses || $isGenerators || $isActivations || apply_filters( 'dlm_admin_stylesheet_is_manage', false );
 
 		/**
@@ -214,9 +215,9 @@ class Boot extends Singleton {
 		/**
 		 * Page: Licenses
 		 */
-		if ( $isLicenses ) {
-			$dateFormat =  get_option( 'date_format' );
-			$timeFormat =  DateFormatter::convertTimeFormatForFlatpickr(get_option('time_format'));
+		if ( $isLicenses || $isOrder ) {
+			$dateFormat = get_option( 'date_format' );
+			$timeFormat = DateFormatter::convertTimeFormatForFlatpickr( get_option( 'time_format' ) );
 
 			wp_enqueue_style( 'dlm_select' );
 			wp_enqueue_style( 'dlm_flatpickr' );
@@ -233,7 +234,7 @@ class Boot extends Singleton {
 					'placeholderFilterByOrder'   => __( 'Filter by order', 'digital-license-manager' ),
 					'placeholderFilterByProduct' => __( 'Filter by product', 'digital-license-manager' ),
 					'placeholderFilterByUser'    => __( 'Filter by user', 'digital-license-manager' ),
-					'dateTimeFormat'                 => sprintf('%s at %s', $dateFormat, $timeFormat),
+					'dateTimeFormat'             => sprintf( '%s at %s', $dateFormat, $timeFormat ),
 				)
 			);
 			wp_localize_script(
