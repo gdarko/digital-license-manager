c9b6f70454526930aefed05ea1365a0ba7e80a7f
Darko Gjorgjijoski <dg@darkog.com>
2023-02-13 01:50:22 +0100

    Declare support for WooCommerce High Performance Order Tables

--- digital-license-manager.php
+++ digital-license-manager.php
@@ -47,6 +47,12 @@ if ( ! defined( 'DLM_PLUGIN_URL' ) ) {
 
 require_once DLM_ABSPATH . 'vendor/autoload.php';
 
+add_action( 'before_woocommerce_init', function() {
+	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+	}
+} );
+
 if ( ! function_exists( 'digital_license_manager' ) ) {
 	function digital_license_manager() {
 		return IdeoLogix\DigitalLicenseManager\Boot::instance();
