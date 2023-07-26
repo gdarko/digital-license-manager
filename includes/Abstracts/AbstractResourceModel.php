3240ab6aef8b8c991dc90dfa9dcdfe358551c97c
Darko Gjorgjijoski <dg@darkog.com>
2023-07-27 01:13:59 +0200

    Deprecate ResourceModel/Repository classes

--- includes/Abstracts/AbstractResourceModel.php
+++ includes/Abstracts/AbstractResourceModel.php
@@ -25,34 +25,10 @@
 
 namespace IdeoLogix\DigitalLicenseManager\Abstracts;
 
-use IdeoLogix\DigitalLicenseManager\Utils\DateFormatter;
-
-defined( 'ABSPATH' ) || exit;
-
-abstract class AbstractResourceModel {
-	/**
-	 * Returns the class properties as an array.
-	 *
-	 * @return array
-	 */
-	public function toArray() {
-		return get_object_vars( $this );
-	}
-
-	/**
-	 * Returns formatted date
-	 *
-	 * @param $column
-	 * @param $srcFormat
-	 * @param string $targetFormat
-	 *
-	 * @return string|null
-	 */
-	public function toFormattedDate( $column, $srcFormat, $targetFormat = 'system' ) {
-		if ( ! isset( $this->$column ) || empty( $this->$column ) ) {
-			return null;
-		}
-
-		return DateFormatter::convert( $this->$column, $srcFormat, $targetFormat );
-	}
+/**
+ * Class AbstractResourceModel
+ * @package IdeoLogix\DigitalLicenseManager\Abstracts
+ * @depreacted  1.5.0
+ */
+abstract class AbstractResourceModel extends AbstractDataModel {
 }
