044822db9fcc88962687c624a1e883204b818b43
Darko Gjorgjijoski <dg@darkog.com>
2023-09-16 18:26:58 +0200

    Exclude gutenberg block sources from zip

--- scripts/release_prepare.sh
+++ scripts/release_prepare.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-PLUGIN_DIR="$( cd -- "$(dirname "$SCRIPT_DIR")" >/dev/null 2>&1 ; pwd -P )"
-PLUGINS_ROOT_DIR="$( cd -- "$(dirname "$PLUGIN_DIR")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPT_DIR="$(
+  cd -- "$(dirname "$0")" >/dev/null 2>&1
+  pwd -P
+)"
+PLUGIN_DIR="$(
+  cd -- "$(dirname "$SCRIPT_DIR")" >/dev/null 2>&1
+  pwd -P
+)"
+PLUGINS_ROOT_DIR="$(
+  cd -- "$(dirname "$PLUGIN_DIR")" >/dev/null 2>&1
+  pwd -P
+)"
 PLUGIN_SLUG=$(basename $PLUGIN_DIR)
 
 if [[ -f "$PLUGIN_DIR/composer.json" ]]; then
@@ -14,19 +23,33 @@ if [ -f "$PLUGINS_ROOT_DIR/$PLUGIN_SLUG.zip" ]; then
   rm "$PLUGINS_ROOT_DIR/$PLUGIN_SLUG.zip"
 fi
 
-cd $PLUGINS_ROOT_DIR
+cd "$PLUGIN_DIR/blocks"
+rm -rf node_modules
+rm -rf package-lock.json
+npm install --legacy-peer-deps
+npm run build
+if [ ! -d "$PLUGIN_DIR/blocks/dist" ]; then
+  echo "Unable to build with NPM"
+  exit
+fi
+
+cd "$PLUGINS_ROOT_DIR"
 
 zip -r "$PLUGIN_SLUG.zip" "$PLUGIN_SLUG" \
- -x="*tests*" \
- -x="*scripts*" \
- -x="*bin*" \
- -x="*.git*" \
- -x="*.phpunit.result.cache*" \
- -x="*.travis.yml*" \
- -x="*composer.json*" \
- -x="*composer.lock*" \
- -x="*phpcs.xml.dist*" \
- -x="*phpunit.xml.dist*" \
- -x="*README.md*"
+  -x="*tests*" \
+  -x="*scripts*" \
+  -x="*bin*" \
+  -x="*blocks/src*" \
+  -x="*blocks/node_modules*" \
+  -x="*blocks/package.json*" \
+  -x="*blocks/package-lock.json*" \
+  -x="*.git*" \
+  -x="*.phpunit.result.cache*" \
+  -x="*.travis.yml*" \
+  -x="*composer.json*" \
+  -x="*composer.lock*" \
+  -x="*phpcs.xml.dist*" \
+  -x="*phpunit.xml.dist*" \
+  -x="*README.md*"
 
 echo "New version ready."
