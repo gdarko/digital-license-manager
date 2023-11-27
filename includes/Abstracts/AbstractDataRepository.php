5fa1f875d6cfbe86d2cf4548ddb636c17457b145
Darko Gjorgjijoski <dg@darkog.com>
2023-11-27 09:10:14 +0100

    Improvements to the count() method in AbstractDataRepository

--- includes/Abstracts/AbstractDataRepository.php
+++ includes/Abstracts/AbstractDataRepository.php
@@ -332,7 +332,11 @@ class AbstractDataRepository implements DataRepositoryInterface {
 	 */
 	public function count( $where = [] ) {
 		try {
-			return $this->buildQuery( $where )->count();
+			if ( count( $where ) > 0 ) {
+				$where = self::buildWhere( $where );
+			}
+			$builder = $this->buildQuery( $where );
+			return $builder->count();
 		} catch ( \Exception $e ) {
 			return 0;
 		}
